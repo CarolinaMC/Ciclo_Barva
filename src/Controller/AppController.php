e266aec4583c63e6a615bdee00f831a94e2c807c
juliusDE <julianjdh18@gmail.com>
2018-03-07 20:32:38 -0600

    cambios daniel

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -45,7 +45,7 @@ class AppController extends Controller
         $this->loadComponent('Flash');
 
         
-       $this->loadComponent('Auth', [
+        $this->loadComponent('Auth', [
                 'authorize'=>['Controller'],
                 'authenticate'=>[
                     'Form'=> [
